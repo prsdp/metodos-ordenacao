53e3d96d1e49f75766353f9f6e172490741227af
Jônatas Pedraza <jonatas.nona@gmail.com>
2013-04-12 00:49:46 -0400

    Suporte a quantidade de vetores a serem ordenados
    * salva um arquivo chamado methods.csv

--- src/sortable.c
+++ src/sortable.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <time.h>
 #include "sorting.h"
 #include "utils.h"
 
@@ -10,16 +11,126 @@ void usage() {
     printf("--method <type>           tipo de ordenação.\n");
     printf("--array-size <size>       [opcional] tamanho do vetor a ser gerado, default é 10.000\n");
     printf("--range <max-range>       [opcional] intervalo dos valores aleatórios, default é 65.535\n");
-    printf("--print                   [opcional] imprime o vetor ordenado ao final da operação\n");
+    printf("--quantity <value>        [opcional] quantidade de vetores a serem criados, default é 1\n");
+    printf("--print-array             [opcional] imprime o vetor ordenado ao final da operação, apenas se --quantity for igual a 1\n");
     printf("--help                    lista as opções do programa.\n\n");
     printf("Tipos de métodos válidos:\n");
     printf("bubble, insertion, selection, quick, heap, count e merge.\n");
 }
 
+void alakazam(int quantity, uint8_t type, int size, uint16_t max_range, uint8_t print_flag) {
+    int i;
+    float sum;
+    time_t start, end;
+    time(&start);
+
+    switch (type) {
+        case 0:
+            printf("*** BUBBLE SORT ***\n");
+            for (i = 1; i <= quantity; i++) {
+                int *array = malloc(sizeof(int) * size);
+                random_array(array, size, max_range);
+                bubble_sort(array, size);
+                if (quantity == 1 && print_flag) {
+                    print_array(array, size);
+                }
+                sum += leakage(array, size);
+                free(array);
+            }
+            break;
+        case 1:
+            printf("*** INSERTION SORT ***\n");
+            for (i = 1; i <= quantity; i++) {
+                int *array = malloc(sizeof(int) * size);
+                random_array(array, size, max_range);
+                insertion_sort(array, size);
+                if (quantity == 1 && print_flag) {
+                    print_array(array, size);
+                }
+                sum += leakage(array, size);
+                free(array);
+            }
+            break;
+        case 2:
+	        printf("*** SELECTION SORT ***\n");
+            for (i = 1; i <= quantity; i++) {
+                int *array = malloc(sizeof(int) * size);
+                random_array(array, size, max_range);
+                selection_sort(array, size);
+                if (quantity == 1 && print_flag) {
+                    print_array(array, size);
+                }
+                sum += leakage(array, size);
+                free(array);
+            }
+            break;
+        case 3:
+	        printf("*** QUICK SORT ***\n");
+            for (i = 1; i <= quantity; i++) {
+                int *array = malloc(sizeof(int) * size);
+                random_array(array, size, max_range);
+                quick_sort(array, 0, size);
+                if (quantity == 1 && print_flag) {
+                    print_array(array, size);
+                }
+                sum += leakage(array, size);
+                free(array);
+            }
+            break;
+        case 4:
+            printf("*** HEAP SORT ***\n");
+            for (i = 1; i <= quantity; i++) {
+                int *array = malloc(sizeof(int) * size);
+                random_array(array, size, max_range);
+                heap_sort(array, size);
+                if (quantity == 1 && print_flag) {
+                    print_array(array, size);
+                }
+                sum += leakage(array, size);
+                free(array);
+            }
+            break;
+        case 5:
+            printf("*** COUNT SORT ***\n");
+            for (i = 1; i <= quantity; i++) {
+                int *array = malloc(sizeof(int) * size);
+                random_array(array, size, max_range);
+                count_sort(array, size, max(array, size));
+                if (quantity == 1 && print_flag) {
+                    print_array(array, size);
+                }
+                sum += leakage(array, size);
+                free(array);
+            }
+            break;
+        case 6:
+            printf("*** MERGE SORT ***\n");
+            for (i = 1; i <= quantity; i++) {
+                int *array = malloc(sizeof(int) * size);
+                random_array(array, size, max_range);
+                merge_sort(array, size);
+                if (quantity == 1 && print_flag) {
+                    print_array(array, size);
+                }
+                sum += leakage(array, size);
+                free(array);
+            }
+            break;
+        default:
+            break;
+    }
+
+    time(&end);
+    create_file();
+    write_file(size, difftime(end, start), sum);
+
+}
+
 int main (int argc, char *argv[]) {
 
     int size = 10000;
-    int print_flag = 0;
+    int quantity_flag = 1;
+    uint8_t print_flag = 0;
     uint8_t type = -1;
     uint16_t max_range = 65535;
 
@@ -60,6 +171,10 @@ int main (int argc, char *argv[]) {
             max_range = (uint16_t) atoi(argv[argc + 1]);
         }
 
+        if (strcmp(argv[argc], "--quantity") == 0) {
+            quantity_flag = atoi(argv[argc + 1]);
+        }
+
         if (strcmp(argv[argc], "--print-array") == 0) {
             print_flag = 1;
         }
@@ -85,53 +200,8 @@ int main (int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    int *array = malloc(sizeof(int) * size);
-    random_array(array, size, max_range);
-
-    switch (type) {
-        case 0:
-            printf("*** BUBBLE SORT ***");
-            bubble_sort(array, size);
-            printf("\n");
-            break;
-        case 1:
-            printf("*** INSERTION SORT ***");
-            insertion_sort(array, size);
-            printf("\n");
-            break;
-        case 2:
-	        printf("*** SELECTION SORT ***");
-	        selection_sort(array, size);
-	        printf("\n");
-            break;
-        case 3:
-	        printf("*** QUICK SORT ***");
-	        quick_sort(array, 0, size);
-	        printf("\n");
-            break;
-        case 4:
-            printf("*** HEAP SORT ***");
-            heap_sort(array, size);
-            printf("\n");
-            break;
-        case 5:
-            printf("*** COUNT SORT ***");
-            count_sort(array, size, max(array,size));
-            printf("\n");
-            break;
-        case 6:
-            printf("*** MERGE SORT ***");
-            merge_sort(array, size);
-            printf("\n");
-            break;
-        default:
-            break;
-    }
-
-    if (print_flag) {
-        print_array(array, size);
-        printf("\n");
-    }
+    alakazam(quantity_flag, type, size, max_range, print_flag);
+    printf("\n");
 
     return 0;
 }
